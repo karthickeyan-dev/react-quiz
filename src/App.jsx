e1dbf35578c003116d097e64b6443fa8dc102168
Karthic <jkarthickeyan24@gmail.com>
2025-01-20 15:49:31 +0530

    Add Question component and update StartScreen to dispatch start action

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Main from './main';
 import Loader from './Loader';
 import Error from './Error';
 import StartScreen from './StartScreen';
+import Question from './Question';
 
 const initialState = {
   questions: [],
@@ -17,6 +18,8 @@ function reducer(state, action) {
       return { ...state, questions: action.payload, status: 'ready' };
     case 'dataFailed':
       return { ...state, status: 'error' };
+    case 'start':
+      return { ...state, status: 'active' };
   }
 }
 
@@ -36,7 +39,10 @@ export default function App() {
       <Main>
         {status === 'loading' && <Loader />}
         {status === 'error' && <Error />}
-        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
+        {status === 'ready' && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === 'active' && <Question />}
       </Main>
     </div>
   );
--- src/StartScreen.jsx
+++ src/StartScreen.jsx
@@ -1,9 +1,14 @@
-export default function StartScreen({ numQuestions }) {
+export default function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
       <h2>Welcome to the React Quiz!</h2>
       <h3>{numQuestions} questions to test your React mastery</h3>
-      <button className="btn btn-ui">Let's Start</button>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: 'start' })}
+      >
+        Let&apos;s Start
+      </button>
     </div>
   );
 }
